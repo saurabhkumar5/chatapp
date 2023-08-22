283ef739f4c006e6380bed2bf9379684d6c062ff
saurabh kumar <saurabhkumar9493@gmail.com>
2023-08-23 01:33:39 +0530

    chat-app

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBOe97hgDC3ZQs_W73Zlm7tZn6xqhkrhbw",
+  authDomain: "chatapp-6c089.firebaseapp.com",
+  projectId: "chatapp-6c089",
+  storageBucket: "chatapp-6c089.appspot.com",
+  messagingSenderId: "591626814068",
+  appId: "1:591626814068:web:e6b27db9426caf73bd8721"
+};
+
+export const app = initializeApp(firebaseConfig);
